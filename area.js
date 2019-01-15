357f1364a4ce734f04e33a17982aa0a607a5f314
GrassiAlessandro <alessandro.grassi@studenti.unitn.it>
2019-01-15 11:50:22 +0100

    Resolve bug

--- area.js
+++ area.js
@@ -1,5 +1,5 @@
 function getArea(params){
-    if(typeof params === 'array' && params.length === 2 && (params[0] >= 0 && params[1] >= 0)){
+    if(Array.isArray(params) && params.length === 2 && (params[0] >= 0 && params[1] >= 0)){
             return params[0] * params[1]
         }
     else
