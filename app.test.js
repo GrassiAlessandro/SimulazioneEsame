d391b8ff057c191cd0c2eb41680ef6d9bc360b93
GrassiAlessandro <alessandro.grassi@studenti.unitn.it>
2019-01-15 16:17:17 +0100

    Test cases

---
+++ app.test.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+const request = require('supertest'); //allow testing w/ good syntax
+const app = require('./trentose2jan12-server')
+
+test('GET / should return 200', async () => {
+    const response = await request(app).get('/getArea?side1=2&side2=2');
+    expect(response.statusCode).toBe(200);
+});
+test('GET / should return 200', async () => {
+    const response = await request(app).get('/getArea?side1=a&side2=2');
+    expect(response.statusCode).toBe(200);
+});
+test('GET / should return 200', async () => {
+    const response = await request(app).get('/getArea?side1=2&side2=2');
+    expect(response.statusCode).toBe(200);
+});
+test('GET / should return 200', async () => {
+    const response = await request(app).get('/getArea?side1=2&side2=2');
+    expect(response.statusCode).toBe(200);
+});
